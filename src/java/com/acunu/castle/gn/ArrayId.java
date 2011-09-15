d417a7414c0603ee4b76c53cedab00b2e9873068
Grzegorz Miłoś <gregor@acunu.com>
2011-09-15 17:57:03 +0100

    Add missing Id class files.

---
+++ src/java/com/acunu/castle/gn/ArrayId.java
@@ -0,0 +1,12 @@
+package com.acunu.castle.gn;
+
+public class ArrayId {
+    public int daId;
+    public int arrayId;
+
+    public ArrayId(int daId, int arrayId)
+    {
+        this.daId = daId;
+        this.arrayId = arrayId;
+    }
+}
---
+++ src/java/com/acunu/castle/gn/MergeId.java
@@ -0,0 +1,12 @@
+package com.acunu.castle.gn;
+
+public class MergeId {
+    public int daId;
+    public int mergeId;
+
+    public MergeId(int daId, int mergeId)
+    {
+        this.daId = daId;
+        this.mergeId = mergeId;
+    }
+}
