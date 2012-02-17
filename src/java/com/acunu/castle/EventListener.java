8d264e67c1a795d345e4c2546875cbfee731f86d
Grzegorz Miłoś <gregor@acunu.com>
2012-02-17 12:43:10 +0000

    trac-4526. Send an event (callback), when the listener started (finished opening the socket).
    --HG--
    branch : v2
    extra : transplant_source : %281LU%3B%7D%D2%9C%F3%DF%0DSC%FFy%19%8D%ED%FE%3C

--- src/java/com/acunu/castle/EventListener.java
+++ src/java/com/acunu/castle/EventListener.java
@@ -2,7 +2,9 @@ package com.acunu.castle;
 
 public interface EventListener
 {
-	void udevEvent(final String s);
-	
-	void error(Throwable t);
+    void udevEvent(final String s);
+
+    void error(Throwable t);
+
+    void started();
 }
